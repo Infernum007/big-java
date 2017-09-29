6f4f5f3b1d3c1cfa0a624fd83b782501e1191a62
iqtp <ifqthenp@gmail.com>
2017-09-29 23:24:51 +0100

    Add utility method for working with lists

--- early-objects/19-stream-processing/src/main/java/utils/ListUtils.java
+++ early-objects/19-stream-processing/src/main/java/utils/ListUtils.java
@@ -1,6 +1,9 @@
 package utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,6 +22,24 @@ public class ListUtils
     {
         Scanner in = new Scanner(text);
         List<String> words = new ArrayList<>();
+
+        while (in.hasNext()) {
+            String word = in.next().replaceAll("\\W", "");
+            words.add(word);
+        }
+        return words;
+    }
+
+    /**
+     * Gets list of words from a text file. The method strips the words
+     * from any non-word character before adding it to the list.
+     *
+     * @return list of words
+     */
+    public static List<String> getListOfWordsFromTextFile(String path) throws FileNotFoundException
+    {
+        Scanner in = new Scanner(new File(path));
+        List<String> words = new LinkedList<>();
 
         while (in.hasNext()) {
             String word = in.next().replaceAll("\\W", "");
