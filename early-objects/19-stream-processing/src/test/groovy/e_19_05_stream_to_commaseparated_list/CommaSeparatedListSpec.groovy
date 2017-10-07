6f55dbd8bccccad36c7766bb2ae5a31b54d5de76
ifqthenp <ifqthenp@gmail.com>
2017-10-08 00:16:11 +0100

    Change to groovy parametrized test

--- early-objects/19-stream-processing/src/test/groovy/e_19_05_stream_to_commaseparated_list/CommaSeparatedListSpec.groovy
+++ early-objects/19-stream-processing/src/test/groovy/e_19_05_stream_to_commaseparated_list/CommaSeparatedListSpec.groovy
@@ -1,6 +1,7 @@
 package e_19_05_stream_to_commaseparated_list
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * {@code CommaSeparatedStreamSpec} class.
@@ -13,25 +14,22 @@ class CommaSeparatedListSpec extends Specification {
         csl = new CommaSeparatedList()
     }
 
-    def "toString method works with stream of strings"() {
-        given: "stream of strings"
-        def stream = ['Alice', 'was', 'beginning', 'to', 'get', 'very', 'tired'].stream()
+    @Unroll
+    def "#cslResult correctly converted to '#result' limited by #limit"() {
+        when:
+        String cslResult = csl.toString(stream, limit)
 
-        when: "toString method is invoked on the given stream"
-        String commaSeparatedList = csl.toString(stream, 3)
+        then:
+        cslResult == result
 
-        then: "toString method returns comma-separated list of strings"
-        assert commaSeparatedList == 'Alice,was,beginning'
+        where:
+        stream                                              | limit || result
+        ['Alice', 'was', 'beginning', 'to', 'get'].stream() | 4     || 'Alice,was,beginning,to'
+        [0, 3, -5, 4, 12, -6].stream()                      | 3     || '0,3,-5'
+        [0.906, -0.114, 45.14, 4.0, 13.1111].stream()       | 5     || '0.906,-0.114,45.14,4.0,13.1111'
     }
 
-    def "toString method works with stream of Integers"() {
-        given: "stream of integers"
-        def stream = [1, -6, 100, -13, 3, -56].stream()
-
-        when: "toString method is invoked on the given stream"
-        String commaSeparatedList = csl.toString(stream, 5)
-
-        then: "toString method returns comma-separated list of integers"
-        assert commaSeparatedList == '1,-6,100,-13,3'
+    void cleanup() {
+        csl = null
     }
 }
