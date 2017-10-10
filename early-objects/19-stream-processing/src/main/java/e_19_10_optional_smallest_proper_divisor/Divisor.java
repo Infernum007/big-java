81a25494ca4acebd0fcf4bae48c36b5c81bb25e6
ifqthenp <ifqthenp@gmail.com>
2017-10-10 21:13:00 +0100

    Add EO E19.10

---
+++ early-objects/19-stream-processing/src/main/java/e_19_10_optional_smallest_proper_divisor/Divisor.java
@@ -0,0 +1,42 @@
+package e_19_10_optional_smallest_proper_divisor;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Write a method
+ * <pre>{@code
+ *      public static Optional<Integer> smallestProperDivisor(int n)
+ * }</pre>
+ * that returns the smallest proper divisor of {@code n} or, if {@code n}
+ * is a prime number, a value indicating that no result is present.
+ */
+public class Divisor
+{
+    public static void main(String[] args)
+    {
+        int n = 49;
+
+        Optional<Integer> spd = smallestProperDivisor(n);
+
+        if (spd.isPresent()) {
+            System.out.println(spd.get() + " is the smallest divisor of " + n);
+        } else {
+            System.out.println("No result present");
+        }
+    }
+
+    public static Optional<Integer> smallestProperDivisor(int n)
+    {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n is less or equal to 0");
+        }
+
+        try (Stream<Integer> integerStream = Stream.iterate(2, x -> x + 1)) {
+            return integerStream
+                    .limit(n / 2)
+                    .filter(x -> n % x == 0 && !(n == 2))
+                    .findFirst();
+        }
+    }
+}
