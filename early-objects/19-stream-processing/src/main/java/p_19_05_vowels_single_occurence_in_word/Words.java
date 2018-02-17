6087987a113393c5824c07a78304417905d3bf88
ifqthenp <ifqthenp@gmail.com>
2018-02-17 07:16:01 +0000

    Add EO P19.5

---
+++ early-objects/19-stream-processing/src/main/java/p_19_05_vowels_single_occurence_in_word/Words.java
@@ -0,0 +1,46 @@
+package p_19_05_vowels_single_occurence_in_word;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static utils.TextFilePaths.WORDS;
+
+/**
+ * Worked Example 19.1 showed you how to find all words with five distinct
+ * vowels (which might occur more than once). Using a similar approach,
+ * find all words in which each vowel occurs exactly once.
+ */
+public class Words
+{
+    public static void main(String[] args) throws IOException
+    {
+        try (Stream<String> lines = Files.lines(Paths.get(WORDS.toString()))) {
+            List<String> list = lines
+                    .filter(w -> !w.endsWith("'s"))
+                    .filter(Words::hasOnlyFiveDistinctVowels)
+                    .limit(20)
+                    .collect(Collectors.toList());
+
+            list.forEach(System.out::println);
+        }
+    }
+
+    public static boolean hasOnlyFiveDistinctVowels(final String word)
+    {
+        return word.toLowerCase().codePoints()
+                .filter(c -> c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                .sum() == 531; // 531 is a total sum of vowels' codepoints
+    }
+
+    public static boolean hasFiveVowels(final String word)
+    {
+        return word.toLowerCase().codePoints()
+                .filter(c -> c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                .distinct()
+                .count() == 5;
+    }
+}
